8bc3c08910caa896db21b6d6d2d6befb0e927fb5
J3SS3HVA <jesse.leeuwerink@hva.nl>
2026-03-23 17:54:11 +0100

    baisc structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/globals/navbar'
+import Slogan from './components/home/slogan'
+import SeeMore from './components/home/see-others'
+import Product from './components/home/product'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <Slogan />
+      <SeeMore />
+      <Product />
+    </main>
     </>
   )
 }
---
+++ src/components/globals/navbar.jsx
@@ -0,0 +1,9 @@
+const navbar = () => {
+  return (
+    <nav>
+        <a href="">home</a><a href="">shopping</a><a href="">gift card</a>
+    </nav>
+  )
+}
+
+export default navbar
